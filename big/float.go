9b1bbe39a8a002339955c21b8da46959a838df3c
Nick Galbreath <client9@users.noreply.github.com>
2025-09-11 13:24:45 -0700

    update todos

--- big/float.go
+++ big/float.go
@@ -1,11 +1,180 @@
 package big
 
 import (
-	"github.com/client9/bignum/mpfr"
+	stdlib "math/big"
 	"runtime"
 
-	stdlib "math/big"
+	"github.com/client9/bignum/mpfr"
 )
+
+// Zero value is *not* ready to use.
+// It must be created by a New function or a Set function.
+// Otherwise it will panic on nil pointer.
+type Float struct {
+	ptr *mpfr.Float
+
+	// TODO: storing prec and mode natively takes 16 bytes on 64-bit platforms
+	//   * No need for 64-bit values
+	//   * prec should be a uint32
+	//   * mode could also be a uint32
+	// Total: 8 bytes
+	prec uint
+	mode mpfr.RoundMode
+}
+
+func NewFloat(x float64) *Float {
+	var n mpfr.Float
+	mpfr.InitSetD(&n, x, mpfr.RNDN)
+	z := &Float{
+		ptr:  &n,
+		prec: 0,
+		mode: 0,
+	}
+	runtime.AddCleanup(z, mpfr.Clear, &n)
+	return z
+}
+
+// TODO PARSEFLOAT
+
+func (z *Float) Abs(x *Float) *Float {
+	mpfr.Abs(z.ptr, x.ptr, z.mode)
+	return z
+}
+
+// TODO ACC
+
+func (z *Float) Add(x, y *Float) *Float {
+	if z.prec == 0 {
+		z.prec = max(x.prec, y.prec)
+	}
+	mpfr.Add(z.ptr, x.ptr, y.ptr, z.mode)
+	return z
+}
+
+// TODO APPEND
+// TODO APPENDTEXT
+
+// NONSTANDARD
+// TODO make sure mpfr is ok with clearing Nul pointer.
+func (z *Float) Clear() {
+	mpfr.Clear(z.ptr)
+	z.ptr = nil
+	z.prec = 0
+	z.mode = 0
+}
+
+func (x *Float) Cmp(y *Float) int {
+	return mpfr.Cmp(x.ptr, y.ptr)
+}
+
+// TODO COPY
+
+func (z *Float) Float32() float32 {
+	return mpfr.GetFlt(z.ptr, z.mode)
+}
+func (z *Float) Float64() float64 {
+	return mpfr.GetD(z.ptr, z.mode)
+}
+
+// TODO FORMAT
+// TODO GOBDECODE
+// TODO GOBENCODE
+
+func (z *Float) Int() int {
+	return int(mpfr.GetSi(z.ptr, z.mode))
+}
+func (z *Float) Int64() int64 {
+	return int64(mpfr.GetSi(z.ptr, z.mode))
+}
+
+// TODO ISINF
+// TODO ISINT
+// TODO MANTEXP
+// TODO MARSHALTEXT
+// TODO MINPREC
+
+func (z *Float) Mode() stdlib.RoundingMode {
+	return exportRoundingMode(z.mode)
+}
+func (z *Float) Mul(x, y *Float) *Float {
+	if z.prec == 0 {
+		z.prec = max(x.prec, y.prec)
+	}
+	mpfr.Mul(z.ptr, x.ptr, y.ptr, z.mode)
+	return z
+}
+
+func (z *Float) Neg(x *Float) *Float {
+	mpfr.Neg(z.ptr, x.ptr, z.mode)
+	return z
+}
+
+// TODO PARSE
+
+func (z *Float) Prec(prec uint) uint {
+	return z.prec
+}
+
+func (z *Float) Quo(x, y *Float) *Float {
+	if z.prec == 0 {
+		z.prec = max(x.prec, y.prec)
+	}
+	mpfr.Div(z.ptr, x.ptr, y.ptr, z.mode)
+	return z
+}
+
+// TODO RAT
+// TODO SCAN
+// TODO SET
+// TODO SETFLOAT64
+// TODO SETINF
+// TODO SETINT
+// TODO SETINT64
+// TODO SETMANTEXP
+
+func (z *Float) SetMode(mode stdlib.RoundingMode) {
+	z.mode = importRoundingMode(mode)
+}
+
+func (z *Float) SetPrec(prec uint) {
+	mpfr.SetPrec(z.ptr, int(prec))
+	z.prec = prec
+}
+
+// TODO SETRAT
+// TODO SETSTRING
+// TODO SETUINT64
+
+func (x *Float) Sign() int {
+	return mpfr.Sgn(x.ptr)
+}
+
+// TODO SIGNBIT
+
+func (z *Float) Sqrt(x *Float) *Float {
+	if z.prec == 0 {
+		z.prec = x.prec
+	}
+	mpfr.Sqrt(z.ptr, x.ptr, z.mode)
+	return z
+}
+
+func (z *Float) String() string {
+	// matches Go
+	return mpfr.Sprintf3("%.10R*g", z.mode, z.ptr)
+}
+
+func (z *Float) Sub(x, y *Float) *Float {
+	if z.prec == 0 {
+		z.prec = max(x.prec, y.prec)
+	}
+	mpfr.Sub(z.ptr, x.ptr, y.ptr, z.mode)
+	return z
+}
+
+// TODO TEXT
+// TODO UINT64
+// TODO UNMARSHALTEXT
 
 func max(a, b uint) uint {
 	if a >= b {
@@ -48,120 +217,4 @@ func exportRoundingMode(r mpfr.RoundMode) stdlib.RoundingMode {
 	default:
 		panic("unsupported MPFR rounding mode")
 	}
-}
-
-// Zero value is *not* ready to use.  It must be created by a New function or a Set function.
-// Otherwise it will panic on nil pointer.
-type Float struct {
-	ptr  *mpfr.Float
-	prec uint
-	mode mpfr.RoundMode
-}
-
-func NewFloat(x float64) *Float {
-	var n mpfr.Float
-	mpfr.InitSetD(&n, x, mpfr.RNDN)
-	z := &Float{
-		ptr:  &n,
-		prec: 0,
-		mode: 0,
-	}
-	runtime.AddCleanup(z, mpfr.Clear, &n)
-	return z
-}
-func (z *Float) Clear() {
-	mpfr.Clear(z.ptr)
-	z.ptr = nil
-	z.prec = 0
-	z.mode = 0
-}
-
-func (z *Float) Abs(x *Float) *Float {
-	mpfr.Abs(z.ptr, x.ptr, z.mode)
-	return z
-}
-
-func (z *Float) Add(x, y *Float) *Float {
-	if z.prec == 0 {
-		z.prec = max(x.prec, y.prec)
-	}
-	mpfr.Add(z.ptr, x.ptr, y.ptr, z.mode)
-	return z
-}
-
-func (x *Float) Cmp(y *Float) int {
-	return mpfr.Cmp(x.ptr, y.ptr)
-}
-func (z *Float) Float32() float32 {
-	return mpfr.GetFlt(z.ptr, z.mode)
-}
-func (z *Float) Float64() float64 {
-	return mpfr.GetD(z.ptr, z.mode)
-}
-func (z *Float) Int() int {
-	return int(mpfr.GetSi(z.ptr, z.mode))
-}
-func (z *Float) Int64() int64 {
-	return int64(mpfr.GetSi(z.ptr, z.mode))
-}
-func (z *Float) Mode() stdlib.RoundingMode {
-	return exportRoundingMode(z.mode)
-}
-func (z *Float) Mul(x, y *Float) *Float {
-	if z.prec == 0 {
-		z.prec = max(x.prec, y.prec)
-	}
-	mpfr.Mul(z.ptr, x.ptr, y.ptr, z.mode)
-	return z
-}
-
-func (z *Float) Neg(x *Float) *Float {
-	mpfr.Neg(z.ptr, x.ptr, z.mode)
-	return z
-}
-
-func (z *Float) SetMode(mode stdlib.RoundingMode) {
-	z.mode = importRoundingMode(mode)
-}
-
-func (z *Float) Prec(prec uint) uint {
-	return z.prec
-}
-
-func (z *Float) Quo(x, y *Float) *Float {
-	if z.prec == 0 {
-		z.prec = max(x.prec, y.prec)
-	}
-	mpfr.Div(z.ptr, x.ptr, y.ptr, z.mode)
-	return z
-}
-
-func (z *Float) SetPrec(prec uint) {
-	mpfr.SetPrec(z.ptr, int(prec))
-	z.prec = prec
-}
-
-func (x *Float) Sign() int {
-	return mpfr.Sgn(x.ptr)
-}
-
-func (z *Float) String() string {
-	// matches Go
-	return mpfr.Sprintf3("%.10R*g", z.mode, z.ptr)
-}
-
-func (z *Float) Sqrt(x *Float) *Float {
-	if z.prec == 0 {
-		z.prec = x.prec
-	}
-	mpfr.Sqrt(z.ptr, x.ptr, z.mode)
-	return z
-}
-
-func (z *Float) Sub(x, y *Float) *Float {
-	if z.prec == 0 {
-		z.prec = max(x.prec, y.prec)
-	}
-	mpfr.Sub(z.ptr, x.ptr, y.ptr, z.mode)
-	return z
 }
--- big/int.go
+++ big/int.go
@@ -5,7 +5,8 @@ import (
 	"runtime"
 )
 
-// Zero value is *not* ready to use.  It must be created by a New function or a Set function.
+// Zero value is *not* ready to use.
+// It must be created by a New function or a Set function.
 // Otherwise it will panic on nil pointer.
 type Int struct {
 	ptr *mpz.Int
@@ -42,6 +43,17 @@ func (z *Int) Add(x, y *Int) *Int {
 	mpz.Add(z.ptr, x.ptr, y.ptr)
 	return z
 }
+
+// TODO AND
+// TODO ANDNOT
+// TODO APPEND
+// TODO APPENDTEXT
+// TODO BINOMIAL
+// TODO BIT
+// TODO BITLEN
+// TODO BITS
+// TODO BYTES
+
 func (z *Int) Cmp(y *Int) *Int {
 	mpz.Cmp(z.ptr, y.ptr)
 	return z
@@ -50,6 +62,9 @@ func (z *Int) CmpAbs(y *Int) *Int {
 	mpz.Cmpabs(z.ptr, y.ptr)
 	return z
 }
+
+// TODO DIV
+// TODO DIVMOD
 
 func (z *Int) Exp(x, y, m *Int) *Int {
 	if y.Sign() <= 0 {
@@ -63,16 +78,57 @@ func (z *Int) Exp(x, y, m *Int) *Int {
 	}
 	return z
 }
+
+// TODO FILLBYTES
+// TODO FLOAT64
+// TODO FORMAT
+// TODO GCD
+// TODO GOBDECODE
+// TODO GOBENCODE
+// TODO INT64
+// TODO ISINT64
+// TODO ISUINT64
+// TODO LSH
+// TODO MARSHALJSON
+// TODO MARSHALTEXT
+// TODO MOD
+// TODO MODINVERSE
+// TODO MODSQRT
+
 func (z *Int) Mul(x, y *Int) *Int {
 	mpz.Mul(z.ptr, x.ptr, y.ptr)
 	return z
 }
+
+// TODO MULRANGE
+// TODO NEG
+// TODO NOT
+// TODO OR
+
 func (z *Int) ProbablyPrime(n int) bool {
 	return mpz.ProbabPrimeP(z.ptr, n) == 1
 }
+
+// TODO QUO
+// TODO QUOREM
+// TODO RAND
+// TODO REM
+// TODO RSH
+// TODO SCAN
+// TODO SET
+// TODO SETBIT
+// TODO SETBITS
+// TODO SETBYTES
+// TODO SETINT64
+// TODO SETSTRING
+// TODO SETUINT64
+
 func (z *Int) Sign() int {
 	return mpz.Sgn(z.ptr)
 }
+
+// TODO SQRT
+
 func (z *Int) String() string {
 	return mpz.GetStr(10, z.ptr)
 }
@@ -80,3 +136,11 @@ func (z *Int) Sub(x, y *Int) *Int {
 	mpz.Sub(z.ptr, x.ptr, y.ptr)
 	return z
 }
+
+// TODO TEXT
+
+// TODO TRAILINGZEROBITS
+// TODO UINT64
+// TODO UNMARSHALJSON
+// TODO UNMARSHALTEXT
+// TODO XOR
