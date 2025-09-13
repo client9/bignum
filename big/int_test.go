b96986abc09c60320cfd16122e52133c047491a5
Nick Galbreath <client9@users.noreply.github.com>
2025-09-12 18:26:53 -0700

    remove junk

--- big/int_test.go
+++ big/int_test.go
@@ -18,22 +18,3 @@ func TestIntZeroValue(t *testing.T) {
 	var a, b Int
 	a.Add(&a, &b)
 }
-
-func BenchmarkInt(b *testing.B) {
-	for b.Loop() {
-		a := NewInt(1000)
-		b := NewInt(100)
-		a.Add(a, b)
-		b.Add(a, b)
-	}
-}
-func BenchmarkIntTmp(b *testing.B) {
-	for b.Loop() {
-		a := NewIntTmp(1000)
-		b := NewIntTmp(100)
-		defer a.Clear()
-		defer b.Clear()
-		a.Add(a, b)
-		b.Add(a, b)
-	}
-}
